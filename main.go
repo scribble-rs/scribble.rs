dff4b7163662ea5fee4c763e7eb9e60931adafbe
Marcel Schramm <marceloschr@googlemail.com>
2020-04-14 22:05:55 +0200

    * portHTTP flag was accidentally renamed * We now print where the port that we listen on came from

--- main.go
+++ main.go
@@ -12,31 +12,30 @@ import (
 )
 
 func main() {
-	portHTTPFlag := flag.Int("portHTTPFlag", -1, "defines the port to be used for http mode")
+	portHTTPFlag := flag.Int("portHTTP", -1, "defines the port to be used for http mode")
 	flag.Parse()
 
 	var portHTTP int
-	if *portHTTPFlag == -1 {
+	if *portHTTPFlag != -1 {
+		portHTTP = *portHTTPFlag
+		log.Printf("Listening on port %d sourced from portHTTP flag.\n", portHTTP)
+	} else {
 		//Support for heroku, as heroku expects applications to use a specific port.
-		envPort, available := os.LookupEnv("PORT")
-		if available {
-			parsed, parseError := strconv.ParseInt(envPort, 10, 16)
-			if parseError == nil {
-				portHTTP = int(parsed)
-			} else {
-				portHTTP = 8080
-			}
+		envPort, _ := os.LookupEnv("PORT")
+		parsed, parseError := strconv.ParseInt(envPort, 10, 16)
+		if parseError == nil {
+			portHTTP = int(parsed)
+			log.Printf("Listening on port %d sourced from PORT environment variable\n", portHTTP)
 		} else {
 			portHTTP = 8080
+			log.Printf("Listening on default port %d\n", portHTTP)
 		}
-	} else {
-		portHTTP = *portHTTPFlag
 	}
 
 	//Setting the seed in order for the petnames to be random.
 	rand.Seed(time.Now().UnixNano())
 
-	log.Printf("Started. Listening on port %d\n", portHTTP)
+	log.Println("Started.")
 
 	//If this ever fails, it will return and print a fatal logger message
 	log.Fatal(communication.Serve(portHTTP))
