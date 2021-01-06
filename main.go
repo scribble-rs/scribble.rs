13729059d5d2c62b32d3afc70571b8b5b9921f1a
Marcel Schramm <marceloschr@googlemail.com>
2021-01-06 21:06:20 +0100

    Improve error and information output when parsing PORT variable.

--- main.go
+++ main.go
@@ -21,15 +21,23 @@ func main() {
 		log.Printf("Listening on port %d sourced from portHTTP flag.\n", portHTTP)
 	} else {
 		//Support for heroku, as heroku expects applications to use a specific port.
-		envPort, _ := os.LookupEnv("PORT")
-		parsed, parseError := strconv.ParseInt(envPort, 10, 16)
-		if parseError == nil {
-			portHTTP = int(parsed)
-			log.Printf("Listening on port %d sourced from PORT environment variable\n", portHTTP)
-		} else {
-			portHTTP = 8080
-			log.Printf("Listening on default port %d\n", portHTTP)
+		envPort, portVarAvailable := os.LookupEnv("PORT")
+		if portVarAvailable {
+			log.Printf("'PORT' environment variable found: '%s'\n", envPort)
+			parsed, parseError := strconv.ParseInt(envPort, 10, 16)
+			if parseError == nil {
+				portHTTP = int(parsed)
+				log.Printf("Listening on port %d sourced from 'PORT' environment variable\n", portHTTP)
+			} else {
+				log.Printf("Error parsing 'PORT' variable: %s\n", parseError)
+				log.Println("Falling back to default port.")
+			}
 		}
+	}
+
+	if portHTTP == 0 {
+		portHTTP = 8080
+		log.Printf("Listening on default port %d\n", portHTTP)
 	}
 
 	//Setting the seed in order for the petnames to be random.
