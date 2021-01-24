09d5b57ad112717b4dcbcf9a3526c181184b2390
Marcel Schramm <marceloschr@googlemail.com>
2021-01-24 20:34:14 +0100

    document remoteAddressToSimpleIP

--- communication/http.go
+++ communication/http.go
@@ -15,6 +15,11 @@ func userFacingError(w http.ResponseWriter, errorMessage string) {
 	}
 }
 
+// remoteAddressToSimpleIP removes unnecessary clutter from the input,
+// reducing it to a simple IPv4. We expect two different formats here.
+// One being http.Request#RemoteAddr (127.0.0.1:12345) and the other
+// being forward headers, which contain brackets, as there's no proper
+// API, but just a string that needs to be parsed.
 func remoteAddressToSimpleIP(input string) string {
 	address := input
 	lastIndexOfDoubleColon := strings.LastIndex(address, ":")
@@ -25,6 +30,8 @@ func remoteAddressToSimpleIP(input string) string {
 	return strings.TrimSuffix(strings.TrimPrefix(address, "["), "]")
 }
 
+// Serve will start an HTTP server listening on the given port.
+// This is a blocking call-
 func Serve(port int) error {
 	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
