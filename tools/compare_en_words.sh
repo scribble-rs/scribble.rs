edb3de5593468247bf2c2fc97a7bdfee793a71e2
maquis196 <chris@maytownsend.co.uk>
2021-02-19 20:36:11 +0000

    scribble-rs/scribble.rs#208 - Script used and pre-sort dictionary after first pass

---
+++ tools/compare_en_words.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+US_DICT="../game/words/en_us"
+
+# For example on install british dict
+# apt-get install wbritish-large
+
+SYSTEM_GB_DICT='/usr/share/dict/british-english-large'
+OUTPUT_GB_DICT="../game/words/en_gb"
+
+PROPER_NOUN_LIST="../game/words/proper_nouns"
+
+FIXLIST="./fixlist"
+
+rm ${FIXLIST} ${OUTPUT_GB_DICT} || true
+
+while IFS= read -r line
+do 
+  grep -x ${line} ${SYSTEM_GB_DICT} # Check it exists in GB list
+  if [[ $? == 0 ]];
+    then
+      echo ${line} >> ${OUTPUT_GB_DICT} # Write to en_gb
+    else
+      echo ${line} >> ${FIXLIST} # Finally write to fixlist if no matches
+  fi
+done < "${US_DICT}"
