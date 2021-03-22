1f83793870c31404dc1d97cf57f4547fac094712
Marcel Schramm <marceloschr@googlemail.com>
2021-03-23 00:40:07 +0100

    Merge init.go into http.go

--- api/http.go
+++ api/http.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -15,6 +16,28 @@ type BasePageConfig struct {
 	// but already host a different website, then your API paths might have to
 	// look like this: painting.com/scribblers/v1.
 	RootPath string `json:"rootPath"`
+}
+
+//In this init hook we initialize all templates that could at some point
+//be needed during the server runtime. If any of the templates can't be
+//loaded, we panic.
+func init() {
+	rootPath, rootPathAvailable := os.LookupEnv("ROOT_PATH")
+	if rootPathAvailable && rootPath != "" {
+		CurrentBasePageConfig.RootPath = rootPath
+	}
+}
+
+// SetupRoutes registers the /v1/ endpoints with the http package.
+func SetupRoutes() {
+	http.HandleFunc(CurrentBasePageConfig.RootPath+"/v1/stats", stats)
+	//The websocket is shared between the public API and the official client
+	http.HandleFunc(CurrentBasePageConfig.RootPath+"/v1/ws", wsEndpoint)
+
+	//These exist only for the public API. We version them in order to ensure
+	//backwards compatibility as far as possible.
+	http.HandleFunc(CurrentBasePageConfig.RootPath+"/v1/lobby", lobbyEndpoint)
+	http.HandleFunc(CurrentBasePageConfig.RootPath+"/v1/lobby/player", enterLobby)
 }
 
 // remoteAddressToSimpleIP removes unnecessary clutter from the input,
