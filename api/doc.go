a77a93f7b053dbfdda95fcf33f466c89988ce5d8
Marcel Schramm <marceloschr@googlemail.com>
2021-07-04 12:10:56 +0200

    Add package documentation for packages api and frontend

---
+++ api/doc.go
@@ -0,0 +1,6 @@
+// Package api the public APIs for both the HTTP and the WS endpoints.
+// These are being used by the official client and can also be used by third
+// party clients. On top of that this package contains some util code regarding
+// http/ws that can be used by other packages. In order to register the
+// endpoints you have to call SetupRoutes.
+package api
---
+++ frontend/doc.go
@@ -0,0 +1,3 @@
+// Package frontend contains the HTTP endpoints for delivering the official
+// web client. In order to register the endpoints you have to call SetupRoutes.
+package frontend
