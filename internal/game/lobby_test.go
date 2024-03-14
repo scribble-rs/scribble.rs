scribble-rs/scribble.rs

commit 0bc2efb30ef38b23deafd82c8ff8df2dc8b81312

Improve drawer score calculation
Scores of disconnected players are now taken into account, if they've
guessed and then disconnected.

internal/game/lobby.go
@@ -272,7 +272,6 @@ func handleMessage(message string, sender *Player, lobby *Lobby) {
 			sender.LastScore = calculateGuesserScore(lobby.hintCount, lobby.hintsLeft, secondsLeft, lobby.DrawingTime)
 			sender.Score += sender.LastScore
 
-			lobby.scoreEarnedByGuessers += sender.LastScore
 			sender.State = Standby
 
 			lobby.Broadcast(&Event{Type: EventTypeCorrectGuess, Data: sender.ID})
@@ -492,7 +491,6 @@ func kickPlayer(lobby *Lobby, playerToKick *Player, playerToKickIndex int) {
 			otherPlayer.Score -= otherPlayer.LastScore
 			otherPlayer.LastScore = 0
 		}
-		lobby.scoreEarnedByGuessers = 0
 
 		advanceLobbyPredefineDrawer(lobby, roundOver, newDrawer)
 	} else {
@@ -580,24 +578,30 @@ func advanceLobbyPredefineDrawer(lobby *Lobby, roundOver bool, newDrawer *Player
 
 	// The drawer can potentially be null if kicked or the game just started.
 	if drawer := lobby.Drawer(); drawer != nil {
-		if lobby.scoreEarnedByGuessers <= 0 {
-			drawer.LastScore = 0
-		} else {
-			// Average score, but minus one player, since the own score is 0 and doesn't count.
-			playerCount := lobby.GetConnectedPlayerCount()
-			// If the drawer isn't connected though, we mustn't subtract from the count.
-			if drawer.Connected {
-				playerCount--
+		// The drawer can get points even if disconnected. But if they are
+		// connected, we need to ignore them when calculating their score.
+		var (
+			playerCount int
+			scoreSum    int
+		)
+		for _, player := range lobby.GetPlayers() {
+			if player != drawer &&
+				// If the player has guessed, we want to take them into account,
+				// even if they aren't connected anymore. If the player is
+				// connected, but hasn't guessed, it is still as well, as the
+				// drawing must've not been good enough to be guessable.
+				(player.Connected || player.LastScore > 0) {
+				scoreSum += player.LastScore
 			}
-
-			var averageScore int
-			if playerCount > 0 {
-				averageScore = lobby.scoreEarnedByGuessers / playerCount
-			}
-
-			drawer.LastScore = averageScore
-			drawer.Score += drawer.LastScore
 		}
+
+		var averageScore int
+		if playerCount > 0 {
+			averageScore = scoreSum / playerCount
+		}
+
+		drawer.LastScore = averageScore
+		drawer.Score += drawer.LastScore
 	}
 
 	// We need this for the next-turn / game-over event, in order to allow the
@@ -609,7 +613,6 @@ func advanceLobbyPredefineDrawer(lobby *Lobby, roundOver bool, newDrawer *Player
 	if lobby.DrawingTimeNew != 0 {
 		lobby.DrawingTime = lobby.DrawingTimeNew
 	}
-	lobby.scoreEarnedByGuessers = 0
 
 	for _, otherPlayer := range lobby.players {
 		// If the round ends and people still have guessing, that means the

internal/game/data.go
@@ -64,8 +64,7 @@ type Lobby struct {
 	// This is a UTC unix-timestamp in milliseconds.
 	roundEndTime int64
 
-	timeLeftTicker        *time.Ticker
-	scoreEarnedByGuessers int
+	timeLeftTicker *time.Ticker
 	// currentDrawing represents the state of the current canvas. The elements
 	// consist of LineEvent and FillEvent. Please do not modify the contents
 	// of this array an only move AppendLine and AppendFill on the respective

commit f6f2596552fe5eaab53e4cd8f2895a6eb0275f9f

Fix drawer score calculation
Commit 0bc2efb30ef38b23deafd82c8ff8df2dc8b81312 broke this. There's now
a regression test.

internal/game/lobby.go
@@ -565,6 +565,33 @@ func handleNameChangeEvent(caller *Player, lobby *Lobby, name string) {
 	}
 }
 
+func (lobby *Lobby) calculateDrawerScore() int {
+	// The drawer can get points even if disconnected. But if they are
+	// connected, we need to ignore them when calculating their score.
+	var (
+		playerCount int
+		scoreSum    int
+	)
+	for _, player := range lobby.GetPlayers() {
+		if player.State != Drawing &&
+			// If the player has guessed, we want to take them into account,
+			// even if they aren't connected anymore. If the player is
+			// connected, but hasn't guessed, it is still as well, as the
+			// drawing must've not been good enough to be guessable.
+			(player.Connected || player.LastScore > 0) {
+			scoreSum += player.LastScore
+			playerCount++
+		}
+	}
+
+	var averageScore int
+	if playerCount > 0 {
+		averageScore = scoreSum / playerCount
+	}
+
+	return averageScore
+}
+
 // advanceLobbyPredefineDrawer is required in cases where the drawer is removed
 // from the game.
 func advanceLobbyPredefineDrawer(lobby *Lobby, roundOver bool, newDrawer *Player) {
@@ -578,30 +605,9 @@ func advanceLobbyPredefineDrawer(lobby *Lobby, roundOver bool, newDrawer *Player
 
 	// The drawer can potentially be null if kicked or the game just started.
 	if drawer := lobby.Drawer(); drawer != nil {
-		// The drawer can get points even if disconnected. But if they are
-		// connected, we need to ignore them when calculating their score.
-		var (
-			playerCount int
-			scoreSum    int
-		)
-		for _, player := range lobby.GetPlayers() {
-			if player != drawer &&
-				// If the player has guessed, we want to take them into account,
-				// even if they aren't connected anymore. If the player is
-				// connected, but hasn't guessed, it is still as well, as the
-				// drawing must've not been good enough to be guessable.
-				(player.Connected || player.LastScore > 0) {
-				scoreSum += player.LastScore
-			}
-		}
-
-		var averageScore int
-		if playerCount > 0 {
-			averageScore = scoreSum / playerCount
-		}
-
-		drawer.LastScore = averageScore
-		drawer.Score += drawer.LastScore
+		newDrawerScore := lobby.calculateDrawerScore()
+		drawer.LastScore = newDrawerScore
+		drawer.Score += newDrawerScore
 	}
 
 	// We need this for the next-turn / game-over event, in order to allow the

internal/game/lobby_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/lxzan/gws"
 	easyjson "github.com/mailru/easyjson"
 	"github.com/scribble-rs/scribble.rs/internal/sanitize"
+	"github.com/stretchr/testify/require"
 )
 
 func createLobbyWithDemoPlayers(playercount int) *Lobby {
@@ -380,4 +381,131 @@ func Test_kickDrawer(t *testing.T) {
 	if lobby.Drawer() != chantal {
 		t.Errorf("Drawer should've been c, but was %s", lobby.Drawer().Name)
 	}
+}
+
+func Test_calculateDrawerScore(t *testing.T) {
+	t.Run("only disconnected players, with score", func(t *testing.T) {
+		drawer := &Player{State: Drawing}
+		lobby := Lobby{
+			players: []*Player{
+				drawer,
+				{
+					Connected: false,
+					LastScore: 100,
+				},
+				{
+					Connected: false,
+					LastScore: 200,
+				},
+			},
+		}
+
+		require.Equal(t, 150, lobby.calculateDrawerScore())
+	})
+	t.Run("only disconnected players, with no score", func(t *testing.T) {
+		drawer := &Player{State: Drawing}
+		lobby := Lobby{
+			players: []*Player{
+				drawer,
+				{
+					Connected: false,
+					LastScore: 0,
+				},
+				{
+					Connected: false,
+					LastScore: 0,
+				},
+			},
+		}
+
+		require.Equal(t, 0, lobby.calculateDrawerScore())
+	})
+	t.Run("connected players, but no score", func(t *testing.T) {
+		drawer := &Player{State: Drawing}
+		lobby := Lobby{
+			players: []*Player{
+				drawer,
+				{
+					Connected: true,
+					LastScore: 0,
+				},
+				{
+					Connected: true,
+					LastScore: 0,
+				},
+			},
+		}
+
+		require.Equal(t, 0, lobby.calculateDrawerScore())
+	})
+	t.Run("connected players", func(t *testing.T) {
+		drawer := &Player{State: Drawing}
+		lobby := Lobby{
+			players: []*Player{
+				drawer,
+				{
+					Connected: true,
+					LastScore: 100,
+				},
+				{
+					Connected: true,
+					LastScore: 200,
+				},
+			},
+		}
+
+		require.Equal(t, 150, lobby.calculateDrawerScore())
+	})
+	t.Run("some connected players, some disconnected, some without score", func(t *testing.T) {
+		drawer := &Player{State: Drawing}
+		lobby := Lobby{
+			players: []*Player{
+				drawer,
+				{
+					Connected: true,
+					LastScore: 100,
+				},
+				{
+					Connected: false,
+					LastScore: 200,
+				},
+				{
+					Connected: true,
+					LastScore: 0,
+				},
+				{
+					Connected: false,
+					LastScore: 0,
+				},
+			},
+		}
+
+		require.Equal(t, 100, lobby.calculateDrawerScore())
+	})
+	t.Run("some connected players, some disconnected", func(t *testing.T) {
+		drawer := &Player{State: Drawing}
+		lobby := Lobby{
+			players: []*Player{
+				drawer,
+				{
+					Connected: true,
+					LastScore: 100,
+				},
+				{
+					Connected: true,
+					LastScore: 200,
+				},
+				{
+					Connected: false,
+					LastScore: 300,
+				},
+				{
+					Connected: false,
+					LastScore: 400,
+				},
+			},
+		}
+
+		require.Equal(t, 250, lobby.calculateDrawerScore())
+	})
 }