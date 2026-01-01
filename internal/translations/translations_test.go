d2862b62cbbcaf13d1e7c8e39c50fa21f81c112a
Adi <6841988+DeepSpace2@users.noreply.github.com>
2026-01-01 17:41:19 +0200

    feat: RTL support - fix tests

--- internal/translations/translations_test.go
+++ internal/translations/translations_test.go
@@ -10,8 +10,8 @@ import (
 func Test_noObsoleteKeys(t *testing.T) {
 	for language, translationMap := range translationRegistry {
 		t.Run(fmt.Sprintf("obsolete_keys_check_%s", language), func(t *testing.T) {
-			for key := range translationMap {
-				_, englishContainsKey := DefaultTranslation[key]
+			for key := range translationMap.Dictionary {
+				_, englishContainsKey := DefaultTranslation.Dictionary[key]
 				assert.True(t, englishContainsKey, "key %s is obsolete", key)
 			}
 		})
