cd940d95c516f6fae2092dc2a0c53ede167fae5e
Adi <6841988+DeepSpace2@users.noreply.github.com>
2026-01-03 00:50:49 +0200

    fix: panic on error page load

--- internal/frontend/index.go
+++ internal/frontend/index.go
@@ -276,7 +276,7 @@ func (handler *SSRHandler) ssrCreateLobby(writer http.ResponseWriter, request *h
 	if err != nil {
 		pageData.Errors = append(pageData.Errors, err.Error())
 		if err := pageTemplates.ExecuteTemplate(writer, "index", pageData); err != nil {
-			handler.userFacingError(writer, err.Error())
+			handler.userFacingError(writer, err.Error(), translation)
 		}
 
 		return
